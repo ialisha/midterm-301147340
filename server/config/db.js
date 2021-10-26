ec791413f7be1b103b521dd30895bc38dfedb2d8
Kokila Sangilimuthu <kokilascse@gmail.com>
2021-10-26 11:27:04 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/book_store"
+  "URI": "mongodb+srv://kokila:kokila123@midtermdbserver.jbaoz.mongodb.net/book_store?retryWrites=true&w=majority"
+};
