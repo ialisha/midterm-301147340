1f4caef8331c84aebc9816ccfd30fada2006bf96
Alisha <ialisha009@gmail.com>
2021-10-29 03:05:50 +0530

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,5 @@ module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://localhost/books229"
   //"URI": "mongodb://localhost/book_store"
-  "URI": "mongodb+srv://kokila:kokila123@midtermdbserver.jbaoz.mongodb.net/book_store?retryWrites=true&w=majority"
+  "URI":   "mongodb://alisha:alisha786@cluster0-shard-00-00.hnxs1.mongodb.net:27017,cluster0-shard-00-01.hnxs1.mongodb.net:27017,cluster0-shard-00-02.hnxs1.mongodb.net:27017/book_store?ssl=true&replicaSet=atlas-lqyr8k-shard-0&authSource=admin&retryWrites=true&w=majority"
 };
